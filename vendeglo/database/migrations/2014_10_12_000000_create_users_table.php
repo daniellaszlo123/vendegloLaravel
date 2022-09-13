3f6f59ae30120834bf022eb9f6ee3ff9546f2deb
PIKSSL <purpledog120@gmail.com>
2022-09-13 15:04:40 +0200

    kliens

--- vendeglo/database/migrations/2014_10_12_000000_create_users_table.php
+++ vendeglo/database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\User;
 
 class CreateUsersTable extends Migration
 {
@@ -16,12 +17,18 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cim',255);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
+
+        User::create(['name'=>'Lakatos Nemzibo','cim'=>'Haragos utca 27','email'=>'lakatosmegyamehbe32@gmail.com']);
+        User::create(['name'=>'Molnár Anikó','cim'=>'Glükőz út 132','email'=>'anikomama@citromail.hu']);
+        User::create(['name'=>'Tekerő-Pataki Amondó','cim'=>'Dohány utca 62','email'=>'pataki1@freemail.hu']);
+
+
     }
 
     /**
